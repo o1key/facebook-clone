f9ac9199b12f2e1dc4e7040b459f737f97a397e4
Huy Truong <huy.truong@team.enouvo.com>
2022-09-22 23:43:00 +0700

    backend: setup a MongoDb Atlas Account and connect to  application

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const  cors = require("cors");
 const { readdirSync } = require("fs")
-
+const mongoose = require("mongoose")
 const app = express();
 
+// cors
 const allowed = ["http://localhost:3000","some other domain"]
 function options(req, res) {
   let temp;
@@ -24,8 +25,18 @@ function options(req, res) {
 
 app.use(cors(options))
 
+// routes
 // console.log(readdirSync("./routes"));
 readdirSync("./routes").map(route => app.use("/", require("./routes/" + route)))
+
+// database
+mongoose
+  .connect(process.env.MONGODB_URL, {
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch(error => console.log("error to connecting to database", error), process.exit(1))
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {

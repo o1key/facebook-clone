0b0caa38d04959fc456c1bd02d5d62298c86750e
Huy Truong <huy.truong@team.enouvo.com>
2022-09-24 16:31:51 +0700

    backend: Register , validation data and using regex to validate email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,59 @@
-exports.home = (req, res) => {
-  res.send("welcome from user home")
-}
+const User = require("../models/User");
+const { validateEmail, validateLength } = require("../utils/validation");
+
+exports.register = async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    } = req.body;
+
+    const checkEmail = await User.findOne({ email });
+    if (checkEmail)
+      res.status(400).json({
+        message:
+          "This email address already exists, try with a different email address",
+      });
+
+    if (!validateEmail(email))
+      res.status(400).json({ message: "Invalid email address" });
+
+    if (!validateLength(first_name, 3, 30)) {
+      return res.status(400).json({
+        message: "first name must between 3 and 30 characters.",
+      });
+    }
+    if (!validateLength(last_name, 3, 30)) {
+      return res.status(400).json({
+        message: "last name must between 3 and 30 characters.",
+      });
+    }
+    if (!validateLength(password, 6, 40)) {
+      return res.status(400).json({
+        message: "password must be atleast 6 characters.",
+      });
+    }
+    return;
+    const user = await new User({
+      first_name,
+      last_name,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    }).save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};

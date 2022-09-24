45776df0db43e17cbe8fa625903bc542144a4448
Huy Truong <huy.truong@team.enouvo.com>
2022-09-24 15:55:45 +0700

    backend: fix bugs connceting to database

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,46 @@
 require("dotenv").config();
 const express = require("express");
-const  cors = require("cors");
-const { readdirSync } = require("fs")
-const mongoose = require("mongoose")
+const cors = require("cors");
+const { readdirSync } = require("fs");
+const mongoose = require("mongoose");
 const app = express();
 
+// json
+app.use(express.json());
+app.use(cors(options));
+
 // cors
-const allowed = ["http://localhost:3000","some other domain"]
+const allowed = ["http://localhost:3000", "some other domain"];
 function options(req, res) {
   let temp;
   let origin = req.header("Origin");
   if (allowed.indexOf(origin) > -1) {
-    temp = { 
+    temp = {
       origin: true,
-      optionSuccessStatus:200,
-    }
+      optionSuccessStatus: 200,
+    };
   } else {
     temp = {
       origin: "stupid",
-    }
+    };
   }
-  res(null,temp)
+  res(null, temp);
 }
-
-app.use(cors(options))
-
 // routes
 // console.log(readdirSync("./routes"));
-readdirSync("./routes").map(route => app.use("/", require("./routes/" + route)))
-
+readdirSync("./routes").map((route) =>
+  app.use("/", require("./routes/" + route))
+);
 // database
 mongoose
   .connect(process.env.MONGODB_URL, {
-  useNewUrlParser:true,
-  useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("database connected successfully"))
-  .catch(error => console.log("error to connecting to database", error))
+  .catch((error) => console.log("error to connecting to database", error));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`service is running on ${PORT}... `);
-})
+});

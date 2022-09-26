0c33a31479b34f00d917ba4fe22b1a74943cee8b
Huy Truong <huy.truong@team.enouvo.com>
2022-09-26 22:11:48 +0700

    backend: Login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -71,7 +71,17 @@ exports.register = async (req, res) => {
     );
     const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url);
-    res.json(user);
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success! Please activate your email to start",
+    });
+    // res.json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -94,5 +104,40 @@ exports.activeAccount = async (req, res) => {
     return res.status(200).json({
       message: "Account has been activated",
     });
+  }
+};
+
+// login
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "The email address you entered is not connected to an account",
+      });
+    }
+
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      return res.status(400).json({
+        message: "Invalid credential. Please try again",
+      });
+    }
+
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success! Please activate your email to start",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };

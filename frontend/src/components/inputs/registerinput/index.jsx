cfadd6d0d3c94d81eb9d261248be237bb4794ac8
Huy Truong <huy.truong@team.enouvo.com>
2022-09-30 22:59:27 +0700

    frontend: register from error handing

--- frontend/src/components/inputs/registerinput/index.jsx
+++ frontend/src/components/inputs/registerinput/index.jsx
@@ -1,61 +1,65 @@
-import { useMediaQuery } from "react-responsive";
-import { ErrorMessage, useField } from "formik";
 import "./style.css";
-
+import { useField, ErrorMessage } from "formik";
+import { useMediaQuery } from "react-responsive";
 export default function RegisterInput({ placeholder, bottom, ...props }) {
   const [field, meta] = useField(props);
-  const desktopView = useMediaQuery({
-    query: "(min-width:850px)",
+  const view1 = useMediaQuery({
+    query: "(min-width: 539px)",
   });
+  const view2 = useMediaQuery({
+    query: "(min-width: 850px)",
+  });
+  const view3 = useMediaQuery({
+    query: "(min-width: 3000px)",
+  });
+  const test1 = view3 && field.name === "first_name";
+  const test2 = view3 && field.name === "last_name";
 
   return (
-    <div className="input_wrap">
-      {meta.touched && meta.error && !bottom && (
-        <div
-          className={
-            desktopView ? "input_error input_error_desktop " : "input_error"
-          }
-        >
-          {meta.touched && meta.error && <ErrorMessage name={field.name} />}
-          {meta.touched && meta.error && (
-            <div
-              className={desktopView ? "error_arrow_left" : "error_arrow_top"}
-            />
-          )}
-        </div>
-      )}
+    <div className="input_wrap register_input_wrap">
       <input
         className={meta.touched && meta.error ? "input_error_border" : ""}
+        style={{
+          width: `${
+            view1 && (field.name === "first_name" || field.name === "last_name")
+              ? "100%"
+              : view1 && (field.name === "email" || field.name === "password")
+              ? "370px"
+              : "300px"
+          }`,
+        }}
+        type={field.type}
         name={field.name}
         placeholder={placeholder}
         {...field}
         {...props}
       />
-      {meta.touched && meta.error && bottom && (
+      {meta.touched && meta.error && (
         <div
           className={
-            desktopView ? "input_error input_error_desktop" : "input_error"
+            view3 || view2 ? "input_error input_error_desktop" : "input_error"
           }
+          style={{
+            transform: "translateY(2px)",
+            left: `${test1 ? "-88%" : test2 ? "107%" : ""}`,
+          }}
         >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
             <div
               className={
-                desktopView ? "error_arrow_left" : "error_arrow_bottom"
+                view3 || (view2 && field.name !== "last_name")
+                  ? "error_arrow_left"
+                  : (view3 || view2) && field.name === "last_name"
+                  ? "error_arrow_left"
+                  : (!view3 || view2) && "error_arrow_bottom"
               }
-            />
+            ></div>
           )}
         </div>
       )}
 
-      {meta.touched && meta.error && (
-        <i
-          className="error_icon"
-          style={{
-            top: `${!bottom && !desktopView ? "63%" : "15px"}`,
-          }}
-        />
-      )}
+      {meta.touched && meta.error && <i className="error_icon"></i>}
     </div>
   );
 }

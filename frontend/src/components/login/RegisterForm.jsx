cb233b23bfd0a2279964bb443a31b1e6d8615bc2
Huy Truong <huy.truong@team.enouvo.com>
2022-10-02 17:01:52 +0700

    frontend: Finish register error handling and fix errors

--- frontend/src/components/login/RegisterForm.jsx
+++ frontend/src/components/login/RegisterForm.jsx
@@ -2,21 +2,22 @@ import { Form, Formik } from "formik";
 import { useState } from "react";
 import * as Yup from "yup";
 import RegisterInput from "../inputs/registerinput";
+import DateOfBirthSelect from "./DateOfBirthSelect";
+import GenderSelect from "./GenderSelect";
 
-const userInfo = {
-  first_name: "",
-  last_name: "",
-  email: "",
-  password: "",
-  bYear: new Date().getFullYear(),
-  bMonth: new Date().getMonth() + 1,
-  // getMonth() javascript method returns the month of the Date and values are (0-11) 0 for January and 11 for December
-  bDay: new Date().getDate(),
-  gender: "",
-};
-
-const RegisterForm = () => {
-  const [user, setUser] = useState(userInfo);
+export function RegisterForm() {
+  const userInfos = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    bYear: new Date().getFullYear(),
+    bMonth: new Date().getMonth() + 1,
+    // getMonth() javascript method returns the month of the Date and values are (0-11) 0 for January and 11 for December
+    bDay: new Date().getDate(),
+    gender: "",
+  };
+  const [user, setUser] = useState(userInfos);
   const {
     first_name,
     last_name,
@@ -27,18 +28,20 @@ const RegisterForm = () => {
     bDay,
     gender,
   } = user;
+  const [dateError, setDateError] = useState("");
+  const [genderError, setGenderError] = useState("");
 
+  const yearTemp = new Date().getFullYear();
   const handleRegisterChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
-
-  const years = Array.from(new Array(118), (value, index) => bYear - index);
-  const months = Array.from(new Array(12), (value, index) => 1 + index);
-
-  const getDays = new Date(bYear, bMonth, 0).getDate();
-  const days = Array.from(new Array(getDays), (value, index) => 1 + index);
-
+  const years = Array.from(new Array(108), (val, index) => yearTemp - index);
+  const months = Array.from(new Array(12), (val, index) => 1 + index);
+  const getDays = () => {
+    return new Date(bYear, bMonth, 0).getDate();
+  };
+  const days = Array.from(new Array(getDays()), (val, index) => 1 + index);
   const regExpRegister = new RegExp("^[a-z]+$", "ig");
 
   const registerValidation = Yup.object({
@@ -51,9 +54,9 @@ const RegisterForm = () => {
         "Numbers and special characters are not allowed."
       ),
     last_name: Yup.string()
-      .required("What's your First name?")
-      .min(2, "First name must be between 2 and 16 characters.")
-      .max(16, "First name must be between 2 and 16 characters.")
+      .required("What's your Last name?")
+      .min(2, "Last name name must be between 2 and 16 characters.")
+      .max(16, "Last name name must be between 2 and 16 characters.")
       .matches(
         regExpRegister,
         "Numbers and special characters are not allowed."
@@ -65,30 +68,58 @@ const RegisterForm = () => {
       .email("Enter a valid email address."),
     password: Yup.string()
       .required(
-        "Enter a combination of at least six numbers, letters and punctuation marks(such is ! and &)."
+        "Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &)."
       )
-      .min(2, "First name must be between 2 and 16 characters.")
-      .max(36, "First name must be between 2 and 36 characters."),
+      .min(6, "Password must be atleast 6 characters.")
+      .max(36, "Password can't be more than 36 characters"),
   });
+
   return (
     <div className="blur">
       <div className="register">
         <div className="register_header">
-          <i className="exit_icon" />
+          <i className="exit_icon"></i>
           <span>Sign Up</span>
-          <span>It's quick and easy</span>
+          <span>it's quick and easy</span>
         </div>
         <Formik
           enableReinitialize
-          first_name
-          last_name
-          email
-          password
-          bYear
-          bMonth
-          bDay
-          gender
+          initialValues={{
+            first_name,
+            last_name,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+          }}
           validationSchema={registerValidation}
+          onSubmit={() => {
+            let current_date = new Date();
+            let picked_date = new Date(bYear, bMonth, bDay);
+            let atLeast14 = new Date(years[years.length / 2] + 14, 0, 1);
+            let noMoreThan70 = new Date(years[years.length / 2] + 70, 0, 1);
+            if (current_date - picked_date < atLeast14) {
+              // console.log("14");
+              setDateError(
+                "it looks like you've entered the wrong info.Please make sure that you use your real date of birth(between 14 and 70)"
+              );
+            } else if (current_date - picked_date > noMoreThan70) {
+              // console.log("70");
+              setDateError(
+                "it looks like you've entered the wrong info.Please make sure that you use your real date of birth(between 14 and 70)"
+              );
+            } else if (gender === "") {
+              setDateError("");
+              setGenderError(
+                "Please choose a gender. You can change who can see this later."
+              );
+            } else {
+              setDateError("");
+              setGenderError("");
+            }
+          }}
         >
           {(formik) => (
             <Form className="register_form">
@@ -101,7 +132,7 @@ const RegisterForm = () => {
                 />
                 <RegisterInput
                   type="text"
-                  placeholder="Surname"
+                  placeholder="Last name"
                   name="last_name"
                   onChange={handleRegisterChange}
                 />
@@ -126,74 +157,25 @@ const RegisterForm = () => {
                 <div className="reg_line_header">
                   Date of birth <i className="info_icon"></i>
                 </div>
-                <div className="reg_grid">
-                  <select
-                    name="bDay"
-                    value={bDay}
-                    onChange={handleRegisterChange}
-                  >
-                    {days.map((day) => (
-                      <option key={day}>{day}</option>
-                    ))}
-                  </select>
-                  <select
-                    name="bMonth"
-                    value={bMonth}
-                    onChange={handleRegisterChange}
-                  >
-                    {months.map((month) => (
-                      <option>{month}</option>
-                    ))}
-                  </select>
-                  <select
-                    name="bYear"
-                    value={bYear}
-                    onChange={handleRegisterChange}
-                  >
-                    {years.map((year, i) => (
-                      <option key={year} value={year}>
-                        {year}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                <DateOfBirthSelect
+                  bDay={bDay}
+                  bMonth={bMonth}
+                  bYear={bYear}
+                  days={days}
+                  months={months}
+                  years={years}
+                  handleRegisterChange={handleRegisterChange}
+                  dateError={dateError}
+                />
               </div>
               <div className="reg_col">
                 <div className="reg_line_header">
                   Gender <i className="info_icon"></i>
                 </div>
-                <div className="reg_grid">
-                  <label htmlFor="male">
-                    Male
-                    <input
-                      type="radio"
-                      name="gender"
-                      id="male"
-                      value="male"
-                      onChange={handleRegisterChange}
-                    />
-                  </label>
-                  <label htmlFor="female">
-                    Female
-                    <input
-                      type="radio"
-                      name="gender"
-                      id="female"
-                      value="female"
-                      onChange={handleRegisterChange}
-                    />
-                  </label>
-                  <label htmlFor="custom">
-                    Custom
-                    <input
-                      type="radio"
-                      name="gender"
-                      id="custom"
-                      value="custom"
-                      onChange={handleRegisterChange}
-                    />
-                  </label>
-                </div>
+                <GenderSelect
+                  handleRegisterChange={handleRegisterChange}
+                  genderError={genderError}
+                />
               </div>
               <div className="reg_infos">
                 By clicking Sign Up, you agree to our{" "}
@@ -210,6 +192,6 @@ const RegisterForm = () => {
       </div>
     </div>
   );
-};
+}
 
 export default RegisterForm;
--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,6 +1,6 @@
 import Footer from "../../components/login/Footer";
 import LoginForm from "../../components/login/LoginForm";
-import RegisterForm from "../../components/login/RegisterForm";
+import { RegisterForm } from "../../components/login/RegisterForm";
 import "./style.css";
 
 const Login = () => {

b88895bb90d1ea5f90d9ffe9df13f2a119022f8c
Huy Truong <huy.truong@team.enouvo.com>
2022-10-20 21:37:32 +0700

    frontend + backend: Activate account and fix ascurity risk

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { register, activeAccount, login } = require("../controllers/user");
+const { authUser } = require("../models/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/activate", activeAccount);
+router.post("/activate", authUser, activeAccount);
 router.post("/login", login);
 
 module.exports = router;

16b8bcace634a8d47952222e2d27bed5a2751648
Huy Truong <huy.truong@team.enouvo.com>
2022-11-14 00:41:49 +0700

    frontend+backend: upload images to cloudinary

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { createPost } = require("../controllers/post");
-const { authUser } = require("../middlwares/auth");
+const { authUser } = require("../middleware/auth");
 
 const router = express.Router();
 
